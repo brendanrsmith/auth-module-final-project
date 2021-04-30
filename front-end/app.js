85d7f8935aabc432f35b9b3c6baf171f51eec4c4
Jarrell Houston <jhouston2882@yahoo.com>
2021-04-29 19:29:27 -0500

    signin/signup finished

--- front-end/app.js
+++ front-end/app.js
@@ -1,8 +1,49 @@
-'use strict';
-
-const userName = document.getElementById('userName');
-const password = document.getElementById('password');
 
 
+//Sign Up
 
-form.addEventListener('submit', handleSubmit);
+$("#signup").on("submit", function (e) {
+    e.preventDefault();
+
+    const username = $("#username").val();
+    const password = $("#password").val();
+
+    const userObj = {
+        username,
+        password
+    }
+
+    $.ajax({
+        url: "http://localhost:3000/signup",
+        type: "POST",
+        data: userObj,
+    }).done(function (response) {
+        if (response.status === "success") {
+            window.location.href = "/front-end/signin.html"
+        }
+    })
+})
+
+//Sign in
+
+$("#signin").on("submit", function (e) {
+    e.preventDefault();
+
+    const username = $("#username").val();
+    const password = $("#password").val();
+
+    const encoded = btoa(`${username}:${password}`);
+
+    $.ajax({
+        url: "http://localhost:3000/signin",
+        headers: {
+            authorization: `Basic ${encoded}`
+        },
+        type: "POST",
+    }).done(function (response) {
+        if (response.status === "success") {
+            window.location.href = "/front-end/dashboard.html"
+        }
+
+    })
+})
